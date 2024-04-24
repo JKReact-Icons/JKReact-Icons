a42cf8a20b70ff43d31b221b3a9f664e1e5ce63c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 23:09:11 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+// ---------------------- Bootestap Start-------------------------
+
+// Fill Bootstrap Fill
 export { default as ZeroNumberFill } from './Bootstrap/Fill/ZeroNumberFill';
+
+// outline Bootestrap
 export { default as ZeroNumberOutLine } from './Bootstrap/outline/ZeroNumberOutLine';
+export { default as ZeroNumberSqureOutLine } from './Bootstrap/outline/ZeroNumberSqureOutLine';
+
+// ---------------------- Bootestap END-------------------------
