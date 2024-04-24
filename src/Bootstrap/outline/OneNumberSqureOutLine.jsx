4bd5f4628580cb51e4a1d867d409429a095fd0d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 23:22:17 +0530

    Update OneNumberSqureOutLine.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- src/Bootstrap/outline/OneNumberSqureOutLine.jsx
+++ src/Bootstrap/outline/OneNumberSqureOutLine.jsx
@@ -18,7 +18,4 @@ const OneNumberSqureOutLine = ({ color, size }) => {
 
 export default OneNumberSqureOutLine;
 
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-1-square" viewBox="0 0 16 16">
-  
-</svg>
 
