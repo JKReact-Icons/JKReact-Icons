ceedefe22a06b4d533d77ed733493e23a28b3d8b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-24 23:22:47 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ export { default as OneNumberFill } from './Bootstrap/Fill/OneNumberFill';
 export { default as OneNumberSqureFill } from './Bootstrap/Fill/OneNumberSqureFill';
 
 
-
 // outline Bootestrap
 export { default as ZeroNumberOutLine } from './Bootstrap/outline/ZeroNumberOutLine';
 export { default as ZeroNumberSqureOutLine } from './Bootstrap/outline/ZeroNumberSqureOutLine';
 export { default as OneNumberOutline } from './Bootstrap/outline/OneNumberOutline';
+export { default as OneNumberSqureOutLine } from './Bootstrap/outline/OneNumberSqureOutLine';
 
 // ---------------------- Bootestap END-------------------------
