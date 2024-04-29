4744aafdec807181aabb11b35c0062d97f5ab99d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 21:15:55 +0530

    Update LogoBluetooth.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/Bootstrap/logos/LogoBluetooth.jsx
+++ src/Bootstrap/logos/LogoBluetooth.jsx
@@ -15,7 +15,3 @@ const LogoBluetooth = ({ color, size }) => {
 };
 
 export default LogoBluetooth;
-
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bluetooth" viewBox="0 0 16 16">
-  
-</svg>
