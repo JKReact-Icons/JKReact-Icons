4920a030aab42c4afbd7ba1cf546e59c8075aa3b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 20:31:59 +0530

    Update SixNumberOutline.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/Bootstrap/outline/SixNumberOutline.jsx
+++ src/Bootstrap/outline/SixNumberOutline.jsx
@@ -16,7 +16,3 @@ const SixNumberOutline = ({ color, size }) => {
 };
 
 export default SixNumberOutline;
-
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-6-circle" viewBox="0 0 16 16">
-  
-</svg>
