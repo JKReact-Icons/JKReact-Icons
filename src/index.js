42f21ecf898f5b1d90d5f08274e87253261076dc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-29 20:45:19 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ export { default as SixNumberFill } from './Bootstrap/Fill/SixNumberFill';
 export { default as SixNumberSqureFill } from './Bootstrap/Fill/SixNumberSqureFill';
 export { default as SevenNumberFill } from './Bootstrap/Fill/SevenNumberFill';
 export { default as SevenNumberSqureFill } from './Bootstrap/Fill/SevenNumberSqureFill';
+export { default as EightNumberFill } from './Bootstrap/Fill/EightNumberFill';
 
 
 // outline Bootestrap
