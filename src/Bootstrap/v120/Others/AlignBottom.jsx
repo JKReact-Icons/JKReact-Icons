60bfaa1b5478619ffdf901a00b5fce16a703839b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 08:06:16 +0530

    Update AlignBottom.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/Bootstrap/v120/Others/AlignBottom.jsx
+++ src/Bootstrap/v120/Others/AlignBottom.jsx
@@ -9,11 +9,14 @@ const AlignBottom = ({ color, size }) => {
       width={size}
       height={size}
     >
-       <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2"/>
+         <rect width="4" height="12" x="6" y="1" rx="1"/>
+         <path d="M1.5 14a.5.5 0 0 0 0 1zm13 1a.5.5 0 0 0 0-1zm-13 0h13v-1h-13z"/>
     </svg>
   );
 };
 
 export default AlignBottom;
 
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-align-bottom" viewBox="0 0 16 16">
 
+</svg>
