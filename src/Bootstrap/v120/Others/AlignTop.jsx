81d4baf70c59cf2427dc1a2df6d3b6dff1d3bafb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 16:14:11 +0530

    Update AlignTop.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/Bootstrap/v120/Others/AlignTop.jsx
+++ src/Bootstrap/v120/Others/AlignTop.jsx
@@ -9,7 +9,8 @@ const AlignTop = ({ color, size }) => {
       width={size}
       height={size}
     >
-      <path d="M6 13a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zM1 8a.5.5 0 0 0 .5.5H6v-1H1.5A.5.5 0 0 0 1 8m14 0a.5.5 0 0 1-.5.5H10v-1h4.5a.5.5 0 0 1 .5.5"/>
+        <rect width="4" height="12" rx="1" transform="matrix(1 0 0 -1 6 15)"/>
+        <path d="M1.5 2a.5.5 0 0 1 0-1zm13-1a.5.5 0 0 1 0 1zm-13 0h13v1h-13z"/>
     </svg>
   );
 };
