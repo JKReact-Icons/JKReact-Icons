316dab19a25ad404d509a2ab492001f960ce4eb9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 16:30:22 +0530

    Update LogoBrilliance.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/Bootstrap/v120/Logos/LogoBrilliance.jsx
+++ src/Bootstrap/v120/Logos/LogoBrilliance.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LogoBootstrapReboot = ({ color, size }) => {
+const LogoBrilliance = ({ color, size }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,4 +14,4 @@ const LogoBootstrapReboot = ({ color, size }) => {
   );
 };
 
-export default LogoBootstrapReboot;
+export default LogoBrilliance;
