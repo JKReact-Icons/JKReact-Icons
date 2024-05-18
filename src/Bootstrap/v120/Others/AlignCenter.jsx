b672adb31c7e56d0b323bc08e2541b256a4f0017
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 16:06:21 +0530

    Create AlignCenter.jsx

---
+++ src/Bootstrap/v120/Others/AlignCenter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AlignCenter = ({ color, size }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={color}
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+    >
+         <rect width="4" height="12" x="6" y="1" rx="1"/>
+         <path d="M1.5 14a.5.5 0 0 0 0 1zm13 1a.5.5 0 0 0 0-1zm-13 0h13v-1h-13z"/>
+    </svg>
+  );
+};
+
+export default AlignCenter;
+
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-align-center" viewBox="0 0 16 16">
+  <path d="M8 1a.5.5 0 0 1 .5.5V6h-1V1.5A.5.5 0 0 1 8 1m0 14a.5.5 0 0 1-.5-.5V10h1v4.5a.5.5 0 0 1-.5.5M2 7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+</svg>
