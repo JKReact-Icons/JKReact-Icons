171768541645f27ce1e4b8f85058c0215759be10
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 16:08:38 +0530

    Update AlignEnd.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- src/Bootstrap/v120/Others/AlignEnd.jsx
+++ src/Bootstrap/v120/Others/AlignEnd.jsx
@@ -9,7 +9,8 @@ const AlignEnd = ({ color, size }) => {
       width={size}
       height={size}
     >
-      <path d="M8 1a.5.5 0 0 1 .5.5V6h-1V1.5A.5.5 0 0 1 8 1m0 14a.5.5 0 0 1-.5-.5V10h1v4.5a.5.5 0 0 1-.5.5M2 7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+      <path fill-rule="evenodd" d="M14.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 1 0v-13a.5.5 0 0 0-.5-.5"/>
+      <path d="M13 7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1z"/>
     </svg>
   );
 };
